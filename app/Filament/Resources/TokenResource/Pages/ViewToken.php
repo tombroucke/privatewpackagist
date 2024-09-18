2e529c795aedde7bfc15c381d67982cb3a2ca4f1
Tom Broucke <tom@tombroucke.be>
2024-09-18 14:01:49 +0200

    🧹 chore: Remove unused import

--- app/Filament/Resources/TokenResource/Pages/ViewToken.php
+++ app/Filament/Resources/TokenResource/Pages/ViewToken.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\TokenResource\Pages;
 
 use App\Filament\Resources\TokenResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewToken extends ViewRecord
