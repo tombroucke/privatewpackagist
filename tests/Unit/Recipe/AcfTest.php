56402f7c9e70f2761cc73197cb08b0dcbd6b038f
Tom Broucke <tom@tombroucke.be>
2024-09-19 08:41:51 +0200

    🩹 fix: optimize test

--- tests/Unit/Recipe/AcfTest.php
+++ tests/Unit/Recipe/AcfTest.php
@@ -27,10 +27,7 @@ class AcfTest extends TestCase
         ]);
 
         $this->acf = $package->recipe();
-    }
 
-    public function test_package_version_is_fetched(): void
-    {
         Http::fake([
             'https://connect.advancedcustomfields.com/packages.json' => Http::response([
                 'packages' => [
@@ -40,25 +37,24 @@ class AcfTest extends TestCase
                 ],
             ]),
         ]);
+    }
 
+    public function test_package_version_is_fetched(): void
+    {
         $this->assertEquals('6.3.5', $this->acf->version());
     }
 
     public function test_package_download_link_is_generated(): void
     {
-        Http::fake([
-            'https://connect.advancedcustomfields.com/packages.json' => Http::response([
-                'packages' => [
-                    'wpengine/advanced-custom-fields-pro' => [
-                        '6.3.5' => [],
-                    ],
-                ],
-            ]),
-        ]);
 
         $this->assertEquals(
             'https://connect.advancedcustomfields.com/v2/plugins/download?t=6.3.5&p=pro&k=test_license_key',
             $this->acf->downloadLink(),
         );
+    }
+
+    public function test_package_changelog_is_empty(): void
+    {
+        $this->assertEquals('', $this->acf->changelog());
     }
 }
